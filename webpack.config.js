a0323c8794724e069a1d043d9e1a29084629fcf2
Sarah Connor <sarahchamorro@gmail.com>
2019-02-23 11:30:41 -0300

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "app_bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
